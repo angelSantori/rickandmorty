decc5b6f662828d5e0d9bb31003c7a49291c4dd9
angelSantori <antonyo.da.mm@gmail.com>
2024-05-30 18:39:48 -0700

    Deslizamiento infinito

--- lib/provider/api_provider.dart
+++ lib/provider/api_provider.dart
@@ -3,11 +3,13 @@ import 'package:ricknmorty/models/character_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiProvider with ChangeNotifier {
-  final _baseUrl = 'https://rickandmortyapi.com';
+  final _baseUrl = 'rickandmortyapi.com';
   List<Character> characters = [];
 
-  Future<void> getCharacters() async {
-    final result = await http.get(Uri.parse("$_baseUrl/api/character"));
+  Future<void> getCharacters(int page) async {
+    final result = await http.get(Uri.https(_baseUrl, "/api/character", {
+      'page': page.toString(),
+    }));
     final response = characterResponseFromJson(result.body);
     characters.addAll(response.results!);
     notifyListeners();
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,11 +12,27 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final scrollController = ScrollController();
+  bool isLoading = false;
+  int page = 1;
   @override
   void initState() {
     super.initState();
     final apiProvider = Provider.of<ApiProvider>(context, listen: false);
-    apiProvider.getCharacters();
+    apiProvider.getCharacters(page);
+    scrollController.addListener(() async {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        setState(() {
+          isLoading = true;
+        });
+
+        page++;
+        await apiProvider.getCharacters(page);
+        setState(() {
+          isLoading = false;
+        });
+      }
+    });
   }
 
   @override
@@ -36,6 +52,8 @@ class _HomeScreenState extends State<HomeScreen> {
           child: apiProvider.characters.isNotEmpty
               ? CharacterList(
                   apiProvider: apiProvider,
+                  isLoading: isLoading,
+                  scrollController: scrollController,
                 )
               : Center(
                   child: CircularProgressIndicator(
@@ -48,42 +66,59 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class CharacterList extends StatelessWidget {
   final ApiProvider apiProvider;
+  final ScrollController scrollController;
+  final bool isLoading;
 
-  const CharacterList({super.key, required this.apiProvider});
+  const CharacterList(
+      {super.key,
+      required this.apiProvider,
+      required this.scrollController,
+      required this.isLoading});
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 0.87,
         mainAxisSpacing: 10,
         crossAxisSpacing: 10,
       ),
-      itemCount: apiProvider.characters.length,
+      itemCount: isLoading
+          ? apiProvider.characters.length + 2
+          : apiProvider.characters.length,
+      controller: scrollController,
       itemBuilder: (context, index) {
-        final character = apiProvider.characters[index];
-        return GestureDetector(
-          onTap: () {
-            context.go('/character');
-          },
-          child: Card(
-            child: Column(
-              children: [
-                FadeInImage(
-                    placeholder: const AssetImage('assets/images/portal.gif'),
-                    image: NetworkImage(character.image!)),
-                Text(
-                  character.name!,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                )
-              ],
+        if (index < apiProvider.characters.length) {
+          final character = apiProvider.characters[index];
+          return GestureDetector(
+            onTap: () {
+              context.go('/character');
+            },
+            child: Card(
+              child: Column(
+                children: [
+                  FadeInImage(
+                      placeholder: const AssetImage('assets/images/portal.gif'),
+                      image: NetworkImage(character.image!)),
+                  Text(
+                    character.name!,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        );
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.green[300],
+            ),
+          );
+        }
       },
     );
   }
